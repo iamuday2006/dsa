cd61652ebfb2b26ed34e81690387000bd40be027
iamuday2006 <uday.debnath2306@gmail.com>
2026-07-21 00:19:12 +0530

    Time: 4 ms (41.31%), Space: 32.9 MB (5.87%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,53 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = nums[i] * nums[i];
+    vector<int> doSquare(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] *= arr[i];
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return arr;
+    }
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> neg, pos;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= 0)
+                pos.push_back(nums[i]);
+            else
+                neg.push_back(nums[i]);
+        }
+
+        if (neg.size() == 0) {
+            return doSquare(pos);
+        }
+
+        doSquare(neg);
+        sort(neg.begin(), neg.end());
+        doSquare(pos);
+        int i = 0, j = 0, id = 0;
+        vector<int> finalResult(neg.size() + pos.size());
+
+        while (i < neg.size() && j < pos.size()) {
+            if (neg[i] <= pos[j]) {
+                finalResult[id] = neg[i];
+                id++;
+                i++;
+            } else {
+                finalResult[id] = pos[j];
+                id++;
+                j++;
+            }
+        }
+
+        while (i < neg.size()) {
+            finalResult[id] = neg[i];
+            id++;
+            i++;
+        }
+        while (j < pos.size()) {
+            finalResult[id] = pos[j];
+            id++;
+            j++;
+        }
+        return finalResult;
     }
 };
